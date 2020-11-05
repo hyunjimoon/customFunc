ebc85ca3a0bca2d9bfef75bcd747217b555d0c03
HyunjiMoon <optfor3@gmail.com>
2020-11-05 23:02:25 +0900

    add allow undefined

--- interp1.R
+++ interp1.R
@@ -1,9 +1,8 @@
-clone_cmdstan_stanc3 <- function(
+clone_cmdstan <- function(
   clone_dir,
   cmdstan_url = "http://github.com/stan-dev/cmdstan",
   stan_url = "http://github.com/stan-dev/stan",
   math_url = "http://github.com/hyunjimoon/math",
-  stanc3_url = "http://github.com/hyunjimoon/stanc3",
   cmdstan_branch = "develop",
   stan_branch = "develop",
   math_branch = "feature/interp1",
@@ -12,21 +11,17 @@ clone_cmdstan_stanc3 <- function(
   if (!dir.exists(clone_dir)) {
     dir.create(clone_dir)  
   }
-  cmdstan_dir = paste0(clone_dir, "_cmdstan")
-  stanc3_dir = paste0(clone_dir, "_stanc3")
-  git2r::clone(url = cmdstan_url, branch = cmdstan_branch, local_path = file.path(cmdstan_dir))
-  git2r::clone(url = stan_url, branch = stan_branch, local_path = file.path(cmdstan_dir, "stan"))
-  git2r::clone(url = math_url, branch = math_branch, local_path = file.path(cmdstan_dir, "stan", "lib", "stan_math"))
-  git2r::clone(url = stanc3_url, branch = stanc3_branch, local_path = file.path(stanc3_dir))
+  git2r::clone(url = cmdstan_url, branch = cmdstan_branch, local_path = file.path(clone_dir))
+  git2r::clone(url = stan_url, branch = stan_branch, local_path = file.path(clone_dir, "stan"))
+  git2r::clone(url = math_url, branch = math_branch, local_path = file.path(clone_dir, "stan", "lib", "stan_math"))
   cmdstanr::rebuild_cmdstan(dir = clone_dir, cores = cores)
 }
 
-clone_cmdstan_stanc3("customFunc")
+clone_cmdstan("customFunc_cmdstan")
 
-library("cmdstanr"); library("tidyverse")
-set_cmdstan_path("/Users/hyunjimoon/customFunc/customFunc_cmdstan")
-N = 5
-data <- list(N = N, M = 4, x = seq(0.0, 5.0, length = N), y = sin(x), x_test = c(1.0, 1.7, 2.1, 4.0))
-mod <- cmdstan_model("interp1.stan", quiet = FALSE)
-fit <- mod$sample(data, iter_warmup = 0, iter_sampling = 10, fixed_param = TRUE)
-fit$y_test
+
+library("cmdstanr")
+set_cmdstan_path("customFunc_cmdstan")
+mod <- cmdstan_model("interp1.stan", compile = FALSE)
+modc <- mod$compile(stanc_options = list("allow-undefined" = TRUE))
+fit <- mod$sample(iter_warmup = 0, iter_sampling = 10)
--- interp1.stan
+++ interp1.stan
@@ -1,11 +1,20 @@
-data{
- int N;
- int M;
- real x[N];
- real y[N];
- real x_test[M];
+functions {
+  real[] interp1(real[] x, real[] y, real[] x_test);
 }
 
-generated quantities{
-  real y_test[M] = interp1(x, y, x_test);
+transformed data {
+  real x[2] = { -5.0, 10.0 };
+  real y[2] = { -3.0, 0.0 };
+}
+
+parameters {
+  real<lower = -0.5, upper = 10.0> x_int[1];
+}
+
+transformed parameters {
+  real y_int = interp1(x, y, x_int)[1];
+}
+
+model {
+  x_int ~ normal(0, 5);
 }
